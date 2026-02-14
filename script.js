eeeb4483db606d012b4f29ccd552e549194f0c36
Vivan Sirohi <vivandsirohi.@Vivans-MacBook-Air.local>
2026-02-14 22:16:55 +0530

    Updated layout and pricing section

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+});
